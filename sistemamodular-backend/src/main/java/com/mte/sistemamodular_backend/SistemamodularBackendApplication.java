0a1db3e70ab0d396927bad65e061167f1fd41778
marlon.its.me <marlonte81@gmail.com>
2025-05-13 21:43:19 -0500

    agregue el initializar de spring boot base para el backend

---
+++ sistemamodular-backend/src/main/java/com/mte/sistemamodular_backend/SistemamodularBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mte.sistemamodular_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemamodularBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemamodularBackendApplication.class, args);
+	}
+
+}
