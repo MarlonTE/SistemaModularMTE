d58e4f179d6f15c7a0984f4397ff80a38047feae
marlon.its.me <marlonte81@gmail.com>
2025-05-14 17:48:33 -0500

    feat: define AudioFile entity with id, name, and path attributes

--- sistemamodular-backend/src/main/java/com/mte/audiostreaming/model/AudioFile.java
+++ sistemamodular-backend/src/main/java/com/mte/audiostreaming/model/AudioFile.java
@@ -1,5 +1,16 @@
 package com.mte.audiostreaming.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class AudioFile {
-    
+    @Id
+    private Long id;
+    private String name;
+    private String path;
 }
